2ec5ac2fd842d641a5cf74690597f07731af3438
vanshjoshi <131679742+vanshjoshi@users.noreply.github.com>
2026-03-18 15:21:06 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-echo "Starting deployment..."
+echo "Deploying application..."
 
-# Go to parent directory
-cd /home/ec2-user || exit
-
-echo "Cleaning old application..."
-rm -rf urlshortener/*
-
-# Now go into fresh copied files
 cd /home/ec2-user/urlshortener || exit
 
 echo "Installing dependencies..."
 npm install
 
-echo "Starting application..."
+echo "Starting app..."
 
-# Ensure pm2 exists
 which pm2 || sudo npm install -g pm2
 
-pm2 stop all || true
-pm2 start app.js
+pm2 start app.js || pm2 restart app.js
 pm2 save
 
-echo "Deployment successful!"
+echo "Deployment complete!"
