79206f81887d4c8859d8b6a572e0ef7703084e13
vanshjoshi <131679742+vanshjoshi@users.noreply.github.com>
2026-03-20 10:51:33 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,6 @@
 
 echo "Starting deployment..."
 
-cd /home/ec2-user
-
-# Stop old app (if running)
-pm2 stop all || true
-
-# Remove old code
-rm -rf urlshortener
-
-# (CodeDeploy already copied new files here)
 cd /home/ec2-user/urlshortener
 
 echo "Installing dependencies..."
